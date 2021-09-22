9a2bf2034e3e9971ba7c6b8b771882ff26bbe04f
Denis Khodak <80540019+DeFFeCT139@users.noreply.github.com>
2021-09-22 23:07:59 +0300

    Add files via upload

--- js/script.js.js
+++ js/script.js.js
@@ -1,3 +1,18 @@
-$(document).on('click', '.btn-menu', function(){
-    $('.lev-panel').removeClass('net-panel')
+$(document).ready(function(){
+    let position = 0;
+    const slydeToShow = 5;
+    const slydeToScrol = 1;
+    const conteyner = $('.slider-con');
+    const track = $('.slider-track').style;
+    const bolsh = $('.slider-item3');
+    const item = $('.slider-item');
+    const btnLeft = $('.btn-left');
+    const btnRight = $('.btn-ritgh');
+
+    btnLeft.click(function(){
+
+    })
+    btnRight.click(function(){
+        track.left = parseFloat(track) + 200 + "px";
+    })
 })
