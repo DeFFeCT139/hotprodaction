2496d210d3e151e1fec329dd44dbb9b1e19ecbf8
Denis Khodak <80540019+DeFFeCT139@users.noreply.github.com>
2021-09-24 01:33:34 +0300

    Add files via upload

--- js/script.js.js
+++ js/script.js.js
@@ -1,18 +1,30 @@
-$(document).ready(function(){
-    let position = 0;
-    const slydeToShow = 5;
-    const slydeToScrol = 1;
-    const conteyner = $('.slider-con');
-    const track = $('.slider-track').style;
-    const bolsh = $('.slider-item3');
-    const item = $('.slider-item');
-    const btnLeft = $('.btn-left');
-    const btnRight = $('.btn-ritgh');
-
-    btnLeft.click(function(){
-
-    })
-    btnRight.click(function(){
-        track.left = parseFloat(track) + 200 + "px";
-    })
-})
+$(document).on('click', '.one-block-foot', function () {
+    $('.two-block-foot-last').removeClass('none-rit');
+    $('.free-block-foot-last').removeClass('none-rit');
+    $('.one-block-foot').addClass('her3');
+    $('.one-block-foot-last').addClass('none-rit');
+  });
+  $(document).on('click', '.two-block-foot', function () {
+    $('.two-block-foot-last').addClass('none-rit');
+    $('.two-block-foot').addClass('her2');
+    $('.free-block-foot-last').removeClass('none-rit');
+    $('.one-block-foot-last').removeClass('none-rit');
+  });
+  $(document).on('click', '.free-block-foot', function () {
+    $('.two-block-foot-last').removeClass('none-rit');
+    $('.free-block-foot-last').addClass('none-rit');
+    $('.free-block-foot').addClass('her1');
+    $('.one-block-foot-last').removeClass('none-rit');
+  });
+  $(document).on('click', '.her1', function () {
+    $('.free-block-foot-last').removeClass('none-rit');
+    $('.free-block-foot').removeClass('her1');
+  });
+  $(document).on('click', '.her2', function () {
+    $('.two-block-foot-last').removeClass('none-rit');
+    $('.two-block-foot').removeClass('her2');
+  });
+  $(document).on('click', '.her3', function () {
+    $('.one-block-foot-last').removeClass('none-rit');
+    $('.one-block-foot').removeClass('her3');
+  });
